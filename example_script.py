a699e6b1b5070ac1721eff05b5af6db7d40b35e0
Tamir Budazhapov <tabu00003@stud.uni-saarland.de>
2025-05-07 10:19:09 +0200

    TikTok-scraper fixed

--- example_script.py
+++ example_script.py
@@ -1,10 +1,42 @@
+import os
+import asyncio
+from TikTokApi import TikTokApi
 from TT_Scraper import TT_Scraper
 
-# Configure the scraper, this step is always needed
-tt = TT_Scraper(wait_time=0.3, output_files_fp="test_folder/")
+proxy = {
+    "server": "http://172.81.22.22:29842",
+    "username": "iweber02",
+    "password": "qp9dQbDM",
+}
 
-# Download all metadata as a .json and all content as .mp4/.jpeg
-tt.scrape_list(ids = [7498540589986663726], scrape_content = True, clear_console=True)
+ms_token = os.environ.get("ms_token", None)
 
-# scrape user profile
-tt.scrape_user(username="tagesschau", download_metadata=True)
+
+async def get_video_ids_by_hashtag(hashtag, count=10):
+    async with TikTokApi() as api:
+        await api.create_sessions(
+            ms_tokens=[ms_token],
+            num_sessions=1,
+            sleep_after=3,
+            proxies=[proxy],
+            browser=os.getenv("TIKTOK_BROWSER", "chromium"),
+            headless=False
+        )
+
+        hashtag = api.hashtag(name=hashtag)
+        video_ids = []
+
+        async for video in hashtag.videos(count=count):
+            video_ids.append(video.id)
+
+        return video_ids
+
+
+def fetch_ids_sync(hashtag, count=10):
+    return asyncio.run(get_video_ids_by_hashtag(hashtag, count))
+
+
+# Используем TT_Scraper
+tt = TT_Scraper(wait_time=0.3, output_files_fp="data/")
+ids = fetch_ids_sync("VE80", count=10)
+tt.scrape_list(ids, scrape_content=True, clear_console=True)
